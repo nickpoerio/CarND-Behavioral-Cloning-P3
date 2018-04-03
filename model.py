8e178afad4ab430630e6002ad1e1ed3ffd7320ad
nickpoerio <37049226+nickpoerio@users.noreply.github.com>
2018-04-04 01:09:37 +0200

    model #14

--- model.py
+++ model.py
@@ -86,15 +86,15 @@ history_object = model.fit_generator(train_generator, samples_per_epoch= \
                  len(train_samples), validation_data=validation_generator, \
                  nb_val_samples=len(validation_samples), nb_epoch=3)
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 ### plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
+#plt.plot(history_object.history['loss'])
+#plt.plot(history_object.history['val_loss'])
+#plt.title('model mean squared error loss')
+#plt.ylabel('mean squared error loss')
+#plt.xlabel('epoch')
+#plt.legend(['training set', 'validation set'], loc='upper right')
+#plt.show()
 
 model.save("model.h5")
 
