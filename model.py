0fe72990ba5e1dc83d79b8922656e52baa530597
nickpoerio <37049226+nickpoerio@users.noreply.github.com>
2018-04-06 00:05:01 +0200

    6apr#1

--- model.py
+++ model.py
@@ -52,7 +52,7 @@ from keras.regularizers import l2
 def Preprocessing():
     model = Sequential()
     model.add(Lambda(lambda x: (x/255.)-.5, input_shape=(160,320,3)))
-    model.add(Cropping2D(cropping=((60,20),(0,0))))
+    model.add(Cropping2D(cropping=((50,20),(0,0))))
     return model
 
 def NVIDIAmodel():
